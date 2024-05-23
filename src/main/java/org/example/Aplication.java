98e867cedf4a27b9e70123bf083c53794998a90c
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2024-05-23 20:40:01 +0300

    pe

---
+++ src/main/java/org/example/Aplication.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Aplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aplication.class, args);
+    }
+}
